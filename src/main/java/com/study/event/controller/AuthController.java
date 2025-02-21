abbcb836c1a5cc0325f16a7c680213f67c06c55d
rlawjdgkr <irlawjdgkr@naver.com>
2025-02-21 10:15:07 +0900

    yml git ignore

--- src/main/java/com/study/event/controller/AuthController.java
+++ src/main/java/com/study/event/controller/AuthController.java
@@ -24,6 +24,7 @@ public class AuthController {
         boolean isDuplicate = eventUserService.checkEmailDuplicate(email);
         String message = isDuplicate ? "이메일이 중복되었습니다." : "사용 가능한 이메일입니다.";
 
+
         return ResponseEntity.ok().body(Map.of(
                 "isDuplicate", isDuplicate,
                 "message", message
